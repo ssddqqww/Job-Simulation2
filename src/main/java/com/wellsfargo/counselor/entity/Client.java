6b0750ea22b1fadfb1a7024cd4fe153ad09af69a
Kseniia <kssgubenko@gmail.com>
2026-02-02 13:22:16 +0200

    Update Client.java

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,1 +1,32 @@
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "financial_advisor_id")
+    private FinancialAdvisor financialAdvisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client(String name, String email, String phone, FinancialAdvisor advisor) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.financialAdvisor = advisor;
+    }
+
+    // Геттери та сеттери
+}
 
