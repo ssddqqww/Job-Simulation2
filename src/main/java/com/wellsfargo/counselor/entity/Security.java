9b72b75166033cf89fdd68bd9c776f197d6867d1
Kseniia <kssgubenko@gmail.com>
2026-02-02 13:14:44 +0200

    Update Security.java

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -1,1 +1,37 @@
+@Entity
+public class Security {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String category;
+
+    @Column
+    private LocalDate purchaseDate;
+
+    @Column
+    private Double purchasePrice;
+
+    @Column
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    public Security(String name, String category, LocalDate purchaseDate, Double purchasePrice, Integer quantity, Portfolio portfolio) {
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+        this.portfolio = portfolio;
+    }
+
+    // Геттери та сеттери
+}
 
