01a0cbcec619ff19b89015b80eebe70935436036
Kseniia <kssgubenko@gmail.com>
2026-02-02 13:22:33 +0200

    Update Portfolio.java

--- src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -1,1 +1,21 @@
+@Entity
+public class Portfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL)
+    private List<Security> securities;
+
+    public Portfolio(Client client) {
+        this.client = client;
+        this.securities = new ArrayList<>();
+    }
+
+    // Геттери та сеттери
+}
 
